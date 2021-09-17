a06d36eca7d0aebd2fe37e1d9b44abc764162c7c
antonpettersson <anton.pettersson@live.se>
2021-09-17 09:02:01 +0200

    Re-add netlifyfunctions and update package lock. Also new testrequest to function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,65 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Loader from "react-loader-spinner";
+
+enum LoadingStateTypes {
+  LOADING = "LOADING",
+  NOT_LOADING = "NOT_LOADING",
+  ERROR = "ERROR",
+}
+
+interface ICalendar {
+  calendar: any[];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-  const res = fetch(window.location + '.netlify/functions/calendar').then((r) => {console.log('r: ', r);});
+  const [loadingState, setLoadingState] = useState<LoadingStateTypes>(
+    LoadingStateTypes.NOT_LOADING
+  );
+  const [calendar, setCalendar] = useState<ICalendar | undefined>();
+
+  useEffect(() => {
+    setLoadingState(LoadingStateTypes.LOADING);
+    fetch(window.location + ".netlify/functions/calendar", {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setCalendar(data);
+        setLoadingState(LoadingStateTypes.NOT_LOADING);
+      })
+      .catch((error) => {
+        console.error("Could not fetch calendar data:", error);
+        setLoadingState(LoadingStateTypes.ERROR);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        {loadingState && <Spinner />}
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+const Spinner = () => {
+  return <Loader type="Oval" color="#00BFFF" height={30} />;
+};
+
+export default App;

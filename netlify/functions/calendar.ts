1ef9b790e1e684c9ee1a487fff9726b1d4cb9a2d
antonpettersson <anton.pettersson@live.se>
2021-09-18 13:08:38 +0200

    feat: Displays calendar items

--- netlify/functions/calendar.ts
+++ netlify/functions/calendar.ts
@@ -1,10 +1,34 @@
 import { Handler } from "@netlify/functions";
+import fetch from "node-fetch";
+
+const API_ENDPOINT =
+  "https://www.googleapis.com/calendar/v3/calendars/" +
+  "askcfmm90aj0dee5tu64ngp068@group.calendar.google.com/" +
+  "events?key=";
 
 const handler: Handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
+  try {
+    const response = await fetch(API_ENDPOINT + process.env.VITE_API_KEY, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const data = await response.json();
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+          data,
+        }),
+      };
+  } catch (err: any) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
 };
 
 export { handler };
--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,40 @@ enum LoadingStateTypes {
   ERROR = "ERROR",
 }
 
+interface Data {
+  data: ICalendar;
+}
+
 interface ICalendar {
-  calendar: any[];
+  items: ICalendarItem[];
+}
+
+interface ICalendarItem {
+  start: StartEndData;
+  end: StartEndData;
+  htmlLink: string;
+  summary: string;
+}
+
+interface StartEndData {
+  dateTime: string;
 }
 
 function App() {
   const [loadingState, setLoadingState] = useState<LoadingStateTypes>(
     LoadingStateTypes.NOT_LOADING
   );
-  const [calendar, setCalendar] = useState<ICalendar | undefined>();
+  const [calendar, setCalendar] = useState<ICalendarItem[] | undefined>();
+  const d = Date;
+  const now = new Date(d.now());
 
   useEffect(() => {
     setLoadingState(LoadingStateTypes.LOADING);
     fetch(window.location + ".netlify/functions/calendar", {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
       .then((response) => {
         if (response.ok) {
@@ -34,8 +51,8 @@ function App() {
         }
         throw response;
       })
-      .then((data) => {
-        setCalendar(data);
+      .then((data: Data) => {
+        setCalendar(data.data.items); // TODO: Slice items by three COMING events.
         setLoadingState(LoadingStateTypes.NOT_LOADING);
       })
       .catch((error) => {
@@ -50,9 +67,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        {(loadingState === LoadingStateTypes.NOT_LOADING && calendar) && (
-          <p>boop</p>
-        )}
+        {loadingState === LoadingStateTypes.NOT_LOADING &&
+          calendar &&
+          calendar.map((item) => {
+            return (
+              item &&
+              new Date(item.start.dateTime) > now && (
+                <CalendarItem
+                  summary={item.summary}
+                  start={item.start}
+                  end={item.end}
+                  htmlLink={item.htmlLink}
+                />
+              )
+            );
+          })}
         {loadingState === LoadingStateTypes.LOADING && <Spinner />}
       </header>
     </div>
@@ -61,6 +90,20 @@ function App() {
 
 const Spinner = () => {
   return <Loader type="Oval" color="#00BFFF" height={30} />;
+};
+
+const CalendarItem = (props: ICalendarItem) => {
+  const d = Date;
+  const startDate = new d(props.start.dateTime).toLocaleString();
+  const endDate = new d(props.end.dateTime).toLocaleString();
+
+  return (
+    <div>
+      <p>{props.summary}</p>
+      <p>{startDate}</p>
+      <p>{endDate}</p>
+    </div>
+  );
 };
 
 export default App;
